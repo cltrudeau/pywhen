f63eba5196238ff9ddfa21e85d73622d21ff8eef
Chris <chris@aziiri.com>
2015-09-15 13:50:10 -0400

    Completed implementation of "When"

--- wrench/tests.py
+++ wrench/tests.py
@@ -1,6 +1,7 @@
+from datetime import datetime, date, time
 from unittest import TestCase
 
-from utils import ExtendedEnum
+from utils import ExtendedEnum, When
 
 # =============================================================================
 
@@ -36,3 +37,111 @@ class TestEnum(TestCase):
         self.assertIn('red', names)
         self.assertIn('blue', names)
         self.assertIn('green', names)
+
+        
+class TestWhen(TestCase):
+    def setUp(self):
+        self.full_date = datetime(1972, 1, 31, 13, 55)
+        self.zero_date = datetime(1972, 1, 31, 0, 0)
+        self.only_time = time(13, 55)
+        self.epoch = 65732100
+        self.mepoch = self.epoch * 1000
+        self.micro_epoch = '%s.0000' % (self.epoch)
+
+    def test_constructor(self):
+        self.assertEqual(self.full_date, When(datetime=self.full_date).datetime)
+        self.assertEqual(self.full_date, 
+            When(date=date(1972, 1, 31), time=time(13, 55)).datetime)
+        self.assertEqual(self.full_date, 
+            When(date=date(1972, 1, 31), time_string='13:55').datetime)
+        self.assertEqual(self.full_date, 
+            When(date=date(1972, 1, 31), time_string='13:55:00').datetime)
+
+        self.assertEqual(self.full_date, When(epoch=self.epoch).datetime)
+        self.assertEqual(self.full_date, When(milli_epoch=self.mepoch).datetime)
+
+        # default time
+        self.assertEqual(self.zero_date, When(date=date(1972, 1, 31)).datetime)
+
+        # time only
+        self.assertEqual(self.only_time, When(time=time(13, 55)).time)
+        self.assertEqual(self.only_time, When(time_string='13:55').time)
+
+        # parse detection
+        self.assertEqual(self.zero_date, When(detect='1972-01-31').datetime)
+        self.assertEqual(self.only_time, When(detect='13:55').time)
+        self.assertEqual(self.only_time, When(detect='13:55:00').time)
+        self.assertEqual(self.full_date, 
+            When(detect='1972-01-31 13:55').datetime)
+        self.assertEqual(self.full_date, 
+            When(detect='1972-01-31 13:55:00').datetime)
+        self.assertEqual(self.full_date, 
+            When(detect='1972-01-31T13:55Z').datetime)
+        self.assertEqual(self.full_date, 
+            When(detect='1972-01-31T13:55:00Z').datetime)
+        self.assertEqual(self.full_date, 
+            When(detect='1972-01-31T13:55:00.00Z').datetime)
+
+        # parse force
+        self.assertEqual(self.zero_date, When(parse_date='1972-01-31').datetime)
+        self.assertEqual(self.only_time, When(parse_time='13:55').time)
+        self.assertEqual(self.only_time, When(parse_time_sec='13:55:00').time)
+        self.assertEqual(self.full_date, 
+            When(parse_datetime='1972-01-31 13:55').datetime)
+        self.assertEqual(self.full_date, 
+            When(parse_datetime_sec='1972-01-31 13:55:00').datetime)
+        self.assertEqual(self.full_date, 
+            When(parse_datetime_utc='1972-01-31T13:55Z').datetime)
+        self.assertEqual(self.full_date, 
+            When(parse_datetime_sec_utc='1972-01-31T13:55:00Z').datetime)
+        self.assertEqual(self.full_date, 
+            When(parse_iso_micro='1972-01-31T13:55:00.00Z').datetime)
+
+        # check errors
+        with self.assertRaises(ValueError):
+            When(detect='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_date='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_time='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_time_sec='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_datetime='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_datetime_sec='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_datetime_utc='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_datetime_sec_utc='abc')
+
+        with self.assertRaises(ValueError):
+            When(parse_iso_micro='abc')
+
+        with self.assertRaises(AttributeError):
+            When()
+
+    def test_values(self):
+        when = When(datetime=self.full_date)
+
+        self.assertEqual('1972-01-31', when.string.date)
+        self.assertEqual('13:55', when.string.time)
+        self.assertEqual('13:55:00', when.string.time_sec)
+        self.assertEqual('1972-01-31 13:55', when.string.datetime)
+        self.assertEqual('1972-01-31 13:55:00', when.string.datetime_sec)
+        self.assertEqual('1972-01-31T13:55Z', when.string.datetime_utc)
+        self.assertEqual('1972-01-31T13:55:00Z', when.string.datetime_sec_utc)
+        self.assertEqual('1972-01-31T13:55:00.0Z', when.string.iso_micro)
+
+        self.assertEqual(self.full_date, when.datetime)
+        self.assertEqual(self.full_date.date(), when.date)
+        self.assertEqual(self.only_time, when.time)
+        self.assertEqual(self.epoch, when.epoch)
+        self.assertEqual(self.mepoch, when.milli_epoch)
--- wrench/utils.py
+++ wrench/utils.py
@@ -1,20 +1,29 @@
+import sys
+from datetime import datetime, time
+import time as time_mod
+
+from enum import Enum
 import six
+
+# =============================================================================
+
+# compensate for long disappearing in py3
+if sys.version_info > (3,):
+    long = int
 
 # =============================================================================
 # Extended Enum 
 # =============================================================================
 
-from enum import Enum
-
 class ExtendedEnum(Enum):
-    """Extends Enum (either native or flufl.enum depending on python version)
-    and adds some features.
+    """Extends ``Enum`` (either native or enum34.enum depending on python
+    version) and adds some features.
     """
 
     @classmethod
     def choices(cls):
         """Returns a "choices" list of tuples.  Each member of the list is one
-        of the possible items in the `Enum`, with the first item in the pair
+        of the possible items in the ``Enum``, with the first item in the pair
         being the value and the second the name.  This is compatible with
         django's choices attribute in fields.
 
@@ -47,16 +56,19 @@ class ExtendedEnum(Enum):
 # =============================================================================
 
 class TimeOnlyError(Exception):
+    """Exception indicating that a date operation was attempted on a
+    :class:`When` object that only wraps a python ``time`` instance."""
     pass
 
 
 class When(object):
+    # https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
     """Date/time conversion utility.  A When object wraps either a python
-    `datetime` or `time` instance, providing a common mechanism for building
-    and converting them into different formats.
+    ``datetime`` or ``time`` instance, providing a common mechanism for
+    building and converting them into different formats.
 
-    If When is wrapping a `time` object then some methods will raise a
-    ``class:TimeOnlyError``.
+    If When is wrapping a ``time`` object then some methods will raise a
+    :class:`TimeOnlyError`.
 
     .. _when-formats:
 
@@ -69,8 +81,13 @@ class When(object):
         'datetime_sec': '%Y-%m-%d %H:%M:%S'
         'datetime_utc': '%Y-%m-%dT%H:%MZ'
         'datetime_sec_utc': '%Y-%m-%dT%H:%M:%SZ'
-        'isoformat': '%Y-%m-%dT%H:%M:%S.%fZ'
+        'iso_micro': '%Y-%m-%dT%H:%M:%S.%fZ'
 
+    .. warning::
+
+        All python ``datetime`` objects in this class are naive and have no
+        timezone information associated with them, this includes various
+        formats labelled "utc".
     """
     parse_formats = {
         'date': '%Y-%m-%d',
@@ -80,7 +97,7 @@ class When(object):
         'datetime_sec': '%Y-%m-%d %H:%M:%S',
         'datetime_utc': '%Y-%m-%dT%H:%MZ',
         'datetime_sec_utc': '%Y-%m-%dT%H:%M:%SZ',
-        'isoformat': '%Y-%m-%dT%H:%M:%S.%fZ',
+        'iso_micro': '%Y-%m-%dT%H:%M:%S.%fZ',
     }
 
     class WhenStrformat(object):
@@ -88,50 +105,127 @@ class When(object):
             self.when = when
 
         def __getattribute__(self, name):
-            try:
-                formatter = When.parse_formats[name]
-                return self.when.datetime.strftime(formatter)
+            if name == 'when':
+                return object.__getattribute__(self, name)
 
-            except KeyError:
-                object.__getattribute(self, name)
+            formatter = self.when.parse_formats[name]
+            if name == 'iso_micro':
+                # iso_micro needs special handling as python's %f returns a
+                # zero padded number but the standard doesn't pad the value
+                formatter = self.when.parse_formats['datetime_sec_utc']
+
+            result = self.when.datetime.strftime(formatter)
+
+            if name == 'iso_micro':
+                result = result[:-1] + '.%dZ' % int(
+                    self.when.datetime.strftime('%f'))
+
+            return result
 
 
     def __init__(self, **kwargs):
         """Create a When object.  The constructor accepts a variety of
         keywords depending on what type of date or time information you wish
-        to convert.  Most keywords result in a python `datetime` object being
-        wrapped, but certain cases use a `time` object.  If the When is only a
-        `time` wrapper then some of the methods will not be available.
-        
+        to convert.  Most keywords result in a python ``datetime`` object
+        being wrapped, but certain cases use a ``time`` object.  If the When
+        is only a ``time`` wrapper then some of the methods will not be
+        available.
+
         :param datetime:
-            Create `When` using a python `datetime` object
+            Create ``When`` using a python ``datetime`` object
         :param date:
-            Create `When` using a python `date` object, can be used in
-            conjunction with the `time` keyword.  If used without the `time`
-            keyword the time portion will be set to midnight.
+            Create ``When`` using a python ``date`` object, can be used in
+            conjunction with the ``time`` keyword.  If used without the
+            ``time`` keyword the time portion will be set to midnight.
         :param time:
-            Create `When` using a python `time` object.  If used on its own the
-            date based methods of `When` will not be allowed.  This keyword
-            can be used in conjunction with the `date` keyword to create a
-            fully qualified `When`.
+            Create ``When`` using a python ``time`` object.  If used on its
+            own the date based methods of ``When`` will not be allowed.  This
+            keyword can be used in conjunction with the ``date`` keyword to
+            create a fully qualified ``When``.
         :param time_string:
-            Create `When` using a string containing time information.  Handles
-            either 'hour:minute' or 'hour:minute:second'.  Like the `time`
-            keyword, can be used in conjunction with `date`.
+            Create ``When`` using a string containing time information.
+            Handles either 'hour:minute' or 'hour:minute:second'.  Like the
+            ``time`` keyword, can be used in conjunction with ``date``.
         :param epoch:
-            Create `When` using an integer epoch value
+            Create ``When`` using an integer epoch value
         :param milli_epoch:
-            Create `When` using an integer that is 1000 * an epoch value with
+            Create ``When`` using an integer that is 1000 * an epoch value with
             the last thousands being milli-epoch.
         :param detect:
-            Create `When` by parsing a string which is compared against the
+            Create ``When`` by parsing a string which is compared against the
             list of available string parsers.
         :param parse_*: 
-            Create `When` by parsing a string using the specific 
+            Create ``When`` by parsing a string using the specific 
             :ref:`Supported formats <when-formats>` given.
-            For example, `parse_isoformat` expects the ISO 8601 format.
+            For example, ``parse_iso_micro`` expects the ISO 8601 format.
+
+        :raises ValueError:
+            If a bad string is passed to ``detect`` or ``parse_*`` keywords
+        :raises AttributeError:
+            If the constructor was called without sufficient arguments to
+            result in a date or time being wrapped.
         """
-        pass
+        self._datetime = None
+        self._time = None
+
+        if 'datetime' in kwargs:
+            self._datetime = kwargs['datetime']
+        elif 'date' in kwargs:
+            d = kwargs['date']
+            if 'time' in kwargs:
+                t = kwargs['time']
+            elif 'time_string' in kwargs:
+                t = self._parse_time_string(kwargs['time_string'])
+            else:
+                t = time()
+
+            self._datetime = datetime(d.year, d.month, d.day, t.hour, t.minute,
+                t.second)
+        elif 'time' in kwargs:
+            self._time = kwargs['time']
+        elif 'time_string' in kwargs:
+            self._time = self._parse_time_string(kwargs['time_string'])
+        elif 'epoch' in kwargs:
+            self._datetime = datetime.fromtimestamp(kwargs['epoch'])
+        elif 'milli_epoch' in kwargs:
+            mepoch = kwargs['milli_epoch']
+            epoch = float(mepoch) / 1000.0
+            milli = float(mepoch) - (epoch * 1000.0)
+            self._datetime = datetime.fromtimestamp(long(epoch))
+            self._datetime.replace(microsecond = int(milli / 10.0))
+        elif 'detect' in kwargs:
+            for f in self.parse_formats.values():
+                try:
+                    self._datetime = datetime.strptime(kwargs['detect'], f)
+                    break
+                except ValueError:
+                    # couldn't parse using this format, ignore and try again
+                    pass
+
+            if not self._datetime:
+                # nothing parsed
+                raise ValueError(('could not parse the date/time passed to '
+                    'detect'))
+        else:
+            # loop through all the possible kwargs looking for parse_* keys,
+            # if found parse based on that and stop
+            for key, value in kwargs.items():
+                if key.startswith('parse_'):
+                    f = self.parse_formats[key[6:]]
+                    if key.startswith('parse_time'):
+                        self._time = self._parse_time_string(value)
+                    else:
+                        self._datetime = datetime.strptime(value, f)
+
+                    break
+
+        if not self._datetime and not self._time:
+            raise AttributeError('invalid keyword arguments')
+
+    def _parse_time_string(self, value):
+        parts = value.split(':')
+        parts = [int(p) for p in parts]
+        return time(*parts)
 
     @property
     def string(self):
@@ -140,10 +234,37 @@ class When(object):
 
         Example::
 
-            >>> When(datetime=d).string.isoformat
-            1972-01-31T13:45:00.02
+            >>> When(datetime=d).string.iso_micro
+            1972-01-31T13:45:00.2
 
         """
-
-
         return When.WhenStrformat(self)
+
+    @property
+    def date(self):
+        """Returns a python ``date`` object."""
+        return self._datetime.date()
+
+    @property
+    def datetime(self):
+        """Returns a python ``datetime`` object."""
+        return self._datetime
+
+    @property
+    def time(self):
+        """Returns a python ``time`` object."""
+        if self._datetime:
+            return self._datetime.time()
+
+        return self._time
+
+    @property
+    def epoch(self):
+        """Returns an integer version of epoch, i.e. the number of seconds
+        since Jan 1, 1970."""
+        return long(time_mod.mktime(self._datetime.timetuple()))
+
+    @property
+    def milli_epoch(self):
+        """Returns an int of the epoch * 1000 + milliseconds."""
+        return self.epoch * 1000 + self._datetime.microsecond * 10
