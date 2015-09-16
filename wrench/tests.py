5647ad0aca5338f3f3b22efd1e0282a28e2d1899
Christopher Trudeau <ctrudeau@arsensa.com>
2015-09-16 13:44:32 -0400

    Implement temp_directory() and replaced_directory() Contexts

--- wrench/tests.py
+++ wrench/tests.py
@@ -1,8 +1,9 @@
+import os, shutil, tempfile
 from datetime import datetime, date, time
 from unittest import TestCase
 
 from utils import (ExtendedEnum, When, dynamic_load, camelcase_to_underscore,
-    rows_to_columns)
+    rows_to_columns, temp_directory, replaced_directory)
 
 # =============================================================================
 
@@ -178,3 +179,47 @@ class TestUtils(TestCase):
         ]
 
         self.assertEqual(expected, rows_to_columns(matrix))
+
+class TestContexts(TestCase):
+    def test_temp_dir(self):
+        created_td = ''
+        with temp_directory() as td:
+            created_td = td
+            name = os.path.join(td, 'a.txt')
+            with open(name, 'w') as f:
+                f.write('foo')
+
+            os.path.isfile(name)
+
+        # verify that the temp directory has been cleaned up
+        self.assertFalse(os.path.exists(created_td))
+
+    def test_replace_dir(self):
+        # create a temp directory and put something in it which is to be
+        # replaced
+        test_dir = tempfile.mkdtemp()
+        orig_file = os.path.join(test_dir, 'a.txt')
+        replace_file = os.path.join(test_dir, 'b.txt')
+
+        with open(orig_file, 'w') as f:
+            f.write('foo')
+
+        created_td = ''
+        with replaced_directory(test_dir) as td:
+            created_td = td
+
+            # put something in the replaced directory
+            with open(replace_file, 'w') as f:
+                f.write('bar')
+
+            # original should be moved out of path, replaced should exist
+            self.assertFalse(os.path.exists(orig_file))
+            self.assertTrue(os.path.exists(replace_file))
+
+        # original should be back, replaced should be gone
+        self.assertTrue(os.path.exists(orig_file))
+        self.assertFalse(os.path.exists(replace_file))
+        self.assertFalse(os.path.exists(created_td))
+
+        # cleanup
+        shutil.rmtree(test_dir)
--- wrench/utils.py
+++ wrench/utils.py
@@ -1,4 +1,4 @@
-import sys
+import contextlib, os, shutil, sys, tempfile
 from datetime import datetime, time
 import time as time_mod
 
@@ -312,3 +312,52 @@ def rows_to_columns(matrix):
         data.append([matrix[j][i] for j in range(0, num_rows)])
 
     return data
+
+# =============================================================================
+# Contexts
+# =============================================================================
+
+@contextlib.contextmanager
+def replaced_directory(dirname):
+    """This ``Context`` is used to move the contents of a directory elsewhere
+    temporarily and put them back upon exit.  This allows testing code to use
+    the same file directories as normal code without fear of damage.
+
+    The name of the temporary directory which contains your files is yielded.
+
+    :param dirname:
+        Path name of the directory to be replaced.
+    """
+    if dirname[-1] == '/':
+        dirname = dirname[:-1]
+
+    full_path = os.path.abspath(dirname)
+    if not os.path.isdir(full_path):
+        raise AttributeError('dir_name must be a directory')
+
+    base, name = os.path.split(full_path)
+
+    # create a temporary directory, move provided dir into it and recreate the
+    # directory for the user
+    tempdir = tempfile.mkdtemp()
+    shutil.move(full_path, tempdir)
+    os.mkdir(full_path)
+    try:
+        yield tempdir
+
+    finally:
+        # done context, undo everything
+        shutil.rmtree(full_path)
+        moved = os.path.join(tempdir, name)
+        shutil.move(moved, base)
+        shutil.rmtree(tempdir)
+
+
+@contextlib.contextmanager
+def temp_directory():
+    """This ``Context`` creates a temporary directory and yields its path.
+    Upon exit the directory is removed.
+    """
+    tempdir = tempfile.mkdtemp()
+    yield tempdir
+    shutil.rmtree(tempdir)
